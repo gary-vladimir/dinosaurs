5143e183aa0ff08e1bed167290f370ff60a151f5
Gary Nuñez <yragi.nunez@gmail.com>
2021-03-18 21:36:38 -0600

    the basic structure of dinosaurs cards along with javascript added

--- app.js
+++ app.js
@@ -1,32 +1,47 @@
+// Create Dino Constructor
 
-    // Create Dino Constructor
+// Create Dino Objects
 
+// Create Human Object
 
-    // Create Dino Objects
+// Use IIFE to get human data from form
 
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    // Create Human Object
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    // Use IIFE to get human data from form
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
+// Generate Tiles for each Dino in Array
 
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
+// Add tiles to DOM
 
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
-
-    // Remove form from screen
-
+// Remove form from screen
 
 // On button click, prepare and display infographic
+
+const compareButton = document.getElementById('compareButton');
+const enterHeight = document.getElementById('enterYourHeight');
+const enterWeight = document.getElementById('enterYourWeight');
+const grid = document.getElementById('gridContainer');
+const card = document.querySelector('.card');
+
+compareButton.addEventListener('click', displayGrid);
+
+function changeToImperial() {
+    enterWeight.innerHTML = 'Enter your weight: (lbs)';
+    enterHeight.innerHTML = 'Enter your height: (Feet)';
+}
+
+function changeToMetric() {
+    enterWeight.innerHTML = 'Enter your weight: (kg)';
+    enterHeight.innerHTML = 'Enter your height: (cm)';
+}
+
+function displayGrid() {
+    grid.style.display = 'grid';
+    card.style.display = 'none';
+}
