b0be2038608fc08d2c9ba90c51d379a6f60a00f0
Gary Nuñez <yragi.nunez@gmail.com>
2021-03-19 13:03:21 -0600

    go again button added along with json dino data function and dino constructor

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ const enterWeight = document.getElementById('enterYourWeight');
 const grid = document.getElementById('gridContainer');
 const card = document.querySelector('.card');
 const footer = document.getElementById('footer');
-const goAgainButton = document.getElementById('goAgainButton');
 
 compareButton.addEventListener('click', displayGrid);
 
@@ -47,5 +46,106 @@ function displayGrid() {
     grid.style.display = 'grid';
     card.style.display = 'none';
     footer.style.position = 'unset';
-    goAgainButton.style.display = 'unset';
+}
+
+/**
+ * The raw dinosaur data, put in function to avoid global variable
+ * the function returns an array of dino objects
+ */
+function jsonDinoData() {
+    const dinos = [
+        {
+            species: 'Triceratops',
+            weight: 13000,
+            height: 114,
+            diet: 'herbivore',
+            where: 'North America',
+            when: 'Late Cretaceous',
+            fact: 'First discovered in 1889 by Othniel Charles Marsh',
+        },
+        {
+            species: 'Tyrannosaurus Rex',
+            weight: 11905,
+            height: 144,
+            diet: 'carnivore',
+            where: 'North America',
+            when: 'Late Cretaceous',
+            fact: 'The largest known skull measures in at 5 feet long.',
+        },
+        {
+            species: 'Anklyosaurus',
+            weight: 10500,
+            height: 55,
+            diet: 'herbivore',
+            where: 'North America',
+            when: 'Late Cretaceous',
+            fact: 'Anklyosaurus survived for approximately 135 million years.',
+        },
+        {
+            species: 'Brachiosaurus',
+            weight: 70000,
+            height: '372',
+            diet: 'herbivore',
+            where: 'North America',
+            when: 'Late Jurassic',
+            fact: 'An asteroid was named 9954 Brachiosaurus in 1991.',
+        },
+        {
+            species: 'Stegosaurus',
+            weight: 11600,
+            height: 79,
+            diet: 'herbivore',
+            where: 'North America, Europe, Asia',
+            when: 'Late Jurassic to Early Cretaceous',
+            fact:
+                'The Stegosaurus had between 17 and 22 seperate plates and flat spines.',
+        },
+        {
+            species: 'Elasmosaurus',
+            weight: 16000,
+            height: 59,
+            diet: 'carnivore',
+            where: 'North America',
+            when: 'Late Cretaceous',
+            fact:
+                'Elasmosaurus was a marine reptile first discovered in Kansas.',
+        },
+        {
+            species: 'Pteranodon',
+            weight: 44,
+            height: 20,
+            diet: 'carnivore',
+            where: 'North America',
+            when: 'Late Cretaceous',
+            fact:
+                'Actually a flying reptile, the Pteranodon is not a dinosaur.',
+        },
+        {
+            species: 'Pigeon',
+            weight: 0.5,
+            height: 9,
+            diet: 'herbivore',
+            where: 'Worldwide',
+            when: 'Holocene',
+            fact: 'All birds are living dinosaurs.',
+        },
+    ];
+
+    return dinos;
+}
+
+function DinoConstructor(data, units) {
+    this.diet = data.diet;
+    this.where = data.where;
+    this.species = data.species;
+    this.when = data.when;
+    this.fact = data.fact;
+    //the data comes in imperial so we check units
+    if (units === 'm') {
+        this.weight = Math.round(data.weight / 2.21);
+        this.height = Math.round(data.height * 2.54);
+    } else {
+        this.weight = data.weight;
+        this.height = data.height;
+    }
 }
